bb2717009eeb4c5583b7bb1efd402506e026853e
NORMA MARQUEZ <nemrqz@gmail.com>
2021-03-18 17:05:16 -0600

    Resolución de conflicto

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { usuario } from '../../../shared/usuario.interface';
 
 
 @Component({
